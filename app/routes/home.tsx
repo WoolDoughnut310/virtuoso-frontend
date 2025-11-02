9e8d8bf56d1b4a5a5f8fe9bd0dc670cb80c6acd5
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2025-11-02 10:56:02 +0000

    add button to start concert

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,21 +1,25 @@
-import type { Route } from "./+types/home";
 import WebRTCAudio from "~/components/WebRTCAudio";
 
-export function loader() {
-    return { name: "React Router" };
-}
+export default function Home() {
+    const onClick = async () => {
+        try {
+            const response = await fetch("http://localhost:8000/concerts/start", { method: "POST" });
+            console.log("got", response);
+        } catch (e: any) {
+            console.error("error", e)
+        }
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+    };
+
     return (
-        <div className="text-center p-4">
-            <h1 className="text-2xl">Hello, {loaderData.name}</h1>
-            <a
-                className="block mt-2 text-blue-500 underline hover:text-blue-600"
-                href="https://reactrouter.com/docs"
-            >
-                React Router Docs
-            </a>
+        <div className="p-3">
             <WebRTCAudio />
+            <button
+                onClick={onClick}
+                className="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700 active:scale-95 transition duration-150 font-medium select-none"
+            >
+                Click to Start
+            </button>
         </div>
     );
 }
