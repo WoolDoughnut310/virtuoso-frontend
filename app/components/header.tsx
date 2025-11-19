ed4e723ecbe6e683e3d145c39d1c6cd7a1be3c28
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2025-11-19 16:04:23 +0000

    incorporate app title - Virtuoso

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 import { useIsOwner } from "~/lib/useIsOwner";
 import ViewEditToggle from "./ViewEditToggle";
 
-export default function Header() {
+export default function Header({ title }: { title?: string }) {
     const location = useLocation();
     const currentPath = location.pathname;
     const params = useParams();
@@ -17,26 +17,30 @@ export default function Header() {
     return (
         <div className="flex flex-row px-16 py-8 border-b-4 border-b-[#ccac54] justify-between">
             <h1 className="font-bodoni-moda font-semibold text-6xl">
-                Virtuoso
+                {title || "Virtuoso"}
             </h1>
-            <div className="flex flex-row justify-center items-center gap-16 text-2xl font-ibm-plex-sans font-normal text-[#282828]">
-                {Object.entries(links).map(([name, path]) => {
-                    const isActive = currentPath === path;
+            {!title && (
+                <div className="flex flex-row justify-center items-center gap-16 text-2xl font-ibm-plex-sans font-normal text-[#282828]">
+                    {Object.entries(links).map(([name, path]) => {
+                        const isActive = currentPath === path;
 
-                    return (
-                        <Link
-                            key={path}
-                            to={path}
-                            className={`hover:text-[#C39F45] focus:outline-none ${isActive ? "border-b-2 border-[#C39F45]" : ""}`}
-                        >
-                            {name}
-                        </Link>
-                    );
-                })}
-                {currentPath === links.Discover && <SearchBar />}
-            </div>
+                        return (
+                            <Link
+                                key={path}
+                                to={path}
+                                className={`hover:text-[#C39F45] focus:outline-none ${isActive ? "border-b-2 border-[#C39F45]" : ""}`}
+                            >
+                                {name}
+                            </Link>
+                        );
+                    })}
+                    {currentPath === links.Discover && <SearchBar />}
+                </div>
+            )}
 
-            {matchPath("/concert/*", currentPath) && isOwner && <ViewEditToggle />}
+            {matchPath("/concert/*", currentPath) && isOwner && (
+                <ViewEditToggle />
+            )}
         </div>
     );
 }
