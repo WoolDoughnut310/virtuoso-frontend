af415916927c52d1658135e4c05db4b94877a23d
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2025-11-21 12:10:04 +0000

    implement utility to pick a subsection of an object

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { logoutLogoutPost } from "~/client";
+import queryClient from "./queryClient";
+import { readUsersMeMeGetQueryKey } from "~/client/@tanstack/react-query.gen";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function pick<T extends object, K extends keyof T>(
+    obj: T,
+    keys: readonly K[]
+): Pick<T, K> {
+    return keys.reduce(
+        (acc, key) => {
+            acc[key] = obj[key];
+            return acc;
+        },
+        {} as Pick<T, K>
+    );
+}
+
+export async function logout() {
+    await logoutLogoutPost();
+    queryClient.invalidateQueries({ queryKey: readUsersMeMeGetQueryKey() });
 }
